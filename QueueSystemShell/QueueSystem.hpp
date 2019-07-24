36ae52a049280edbd9be5c066b5d82e64315bfaf
Ricardo Medeiros <j_r_style@hotmail.com>
2019-07-24 03:27:16 -0300

    Initial Definition of QueueSystem abstract interface

--- QueueSystemShell/QueueSystem.hpp
+++ QueueSystemShell/QueueSystem.hpp
@@ -3,12 +3,10 @@
 //STD libraries
 #include <vector>
 #include <iostream>
+#include <string>
 //OtherClasses
 #include "Event.hpp"
-
-
-
-
+#include "Statistic.hpp"
 
 class QueueSystem
 {
@@ -19,7 +17,17 @@ class QueueSystem
 		static int 	shellVerbosity;
 		static int	reportVerbosity;
 		std::vector<Event> events;
+		std::vector<Statistic> statistics;
 
 	public:
+		QueueSystem(int numberOfEvents,long double expectedArrivalInterval,long double expectedServiceTime,long double timeScaler=1);
+		virtual ~QueueSystem()=0;
+		virtual void start(void)=0;
+		virtual void event(int eventNumber)=0;
+		virtual void simulate(void)=0;
+		void report(std::string fileName);
+		void graphicalDemo(bool newSimulation=false,long double timeScaler=1);
+
 };
+
 #endif /* QUEUESYSTEM_HPP_ */
---
+++ QueueSystemShell/Statistic.hpp
@@ -0,0 +1,15 @@
+#ifndef STATISTIC_HPP_
+#define STATISTIC_HPP_
+//STD libraries
+#include <vector>
+
+class Statistic
+{
+	private:
+		std::vector<long double> expectedValues;
+		std::vector<long double> varianceValues;
+		//Still needed interface for basic operations
+	public:
+};
+
+#endif /* STATISTIC_HPP_ */
